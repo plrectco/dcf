40d46ddd30c90b1a5429d02732315e40861d6d5f
plrectco <xy.ou@foxmail.com>
2025-01-11 23:45:21 -0800

    pretty print

--- dcf.py
+++ dcf.py
@@ -74,43 +74,41 @@ def get_terminal_value(stock, years=10):
 def compute_dcf(ticker, years=10):
     stock = yf.Ticker(ticker)
     fcf = get_free_cash_flow(stock)
-    print(f"The FCF for is: {fcf:.2f}")
     discount_rate = get_discount_rate(stock)
-    print(f"The WACC is: {discount_rate:.2f}")
     growth_rate = get_growth_rate(stock)
-    print(f"The growth rate is: {growth_rate:.2f}")
+    print(f"Ticker: {ticker}")
     print(f"FCF: {fcf:.2f}, Discount Rate: {discount_rate:.2f}, Growth Rate: {growth_rate:.2f}")
     
     dcf_value = 0
+    print("*" + "-"*7 + "*" + "-"*19 + "*")
     print(f"| {'Year':<5} | {'FCF (in millions)':<15} |")
-    print("|" + "-"*6 + "|" + "-"*18 + "|")
+    print("|" + "-"*7 + "|" + "-"*19 + "|")
     for year in range(1, years + 1):
         fcf_year = fcf * (1 + growth_rate) ** year
-        print(f"| {year:<5} | {fcf_year/10**6:<15.2f} |")
+        print(f"| {year:<5} | {fcf_year/10**6:<17.2f} |")
         dcf_value += fcf_year / (1 + discount_rate) ** year
+    print("*" + "-"*7 + "*" + "-"*19 + "*")
     terminal_value = get_terminal_value(stock, years)
-    print(f"The terminal value is: {terminal_value/10**6:.2f} million")
     dcf_value += terminal_value
-
     cash = stock.balance_sheet.loc["Cash And Cash Equivalents"].iloc[0]
-    print(f"The cash is : {cash/10**6:.2f} million")
     total_debt = stock.balance_sheet.loc["Total Debt"].iloc[0]
-    print(f"The debt is: {total_debt/10**6:.2f} million")
     dcf_value = dcf_value + cash - total_debt
-    print(f"The total DCF valuation is: {dcf_value/10**6:.2f} million")
+    print(f"Terminal Value: {terminal_value:.2f}, Cash: {cash:.2f}, Total Debt: {total_debt:.2f}")
+    print(f"DCF Value (adjusted): {dcf_value:.2f}")
     share_num = get_share_num(stock)
-    print(f"The number of shares is: {share_num}")
+    print(f"Number of Share: {share_num}")
     dcf_value_per_share = dcf_value / share_num
-    print(f"The DCF valuation per share is: {dcf_value_per_share:.2f}")
+    print(f"The DCF per share value: {dcf_value_per_share:.2f}")
+    print()
     return dcf_value_per_share
 
 def current_sure_value(stock):
     price = stock.info['currentPrice']
     return price
 
-def calculate_margin(ticker):
+def calculate_margin(ticker, years=10):
     stock = yf.Ticker(ticker)
-    dcf_val = compute_dcf(ticker)
+    dcf_val = compute_dcf(ticker, years)
     current_val = current_sure_value(stock)
     return (current_val, round(dcf_val, 2).item(), round((dcf_val - current_val) / current_val, 2).item())
 
@@ -120,11 +118,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Add a variadic argument
     parser.add_argument(
+        "years", 
+        nargs=1,  
+        help="Number of years to project cash flows"
+    )
+    parser.add_argument(
         "tickers",  # Positional argument name
         nargs="*",  # Accepts zero or more arguments
         help="A list of ticker symbols"
     )
     args = parser.parse_args()
-    dcf = [(ticker, calculate_margin(ticker)) for ticker in args.tickers]
+    dcf = [(ticker, calculate_margin(ticker, int(args.years[0]))) for ticker in args.tickers]
     sorted_dcf = sorted(dcf, key=lambda x: x[1][2], reverse=True)
     print(sorted_dcf)
